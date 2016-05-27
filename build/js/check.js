abf1ca13892de61cf23a4cd83e259a4d445e7c98
Michael 'Grraoo' Kosymov <evil@grraoo.ru>
2016-05-27 19:24:42 +0800

    ещё правки

--- build/js/check.js
+++ build/js/check.js
@@ -1,37 +1,28 @@
 var getMessage = function(a, b) {
-	var message = ' ';
-	if(typeof(a)=='boolean') {
-		if (a == true) {
-			message = 'Я попал в ' + b;
-		}
-		else  {
-			message = 'Я никуда не попал';
-		}
-	}
-	else if (typeof(a) == 'number') {
-		message = 'Я прыгнул на ' + a*100 + ' сантиметров';
-	}
+    var message = '';
+    if (typeof a == 'boolean') {
+        if (a == true) {
+            message = 'Я попал в ' + b;
+        } else {
+            message = 'Я никуда не попал';
+        }
+    } else if (typeof a == 'number') {
+        message = 'Я прыгнул на ' + a * 100 + ' сантиметров';
+    } else if ((Object.prototype.toString.call(a) == '[object Array]')) {
+        if ((Object.prototype.toString.call(b) == '[object Array]')) {
+            var length = 0;
+            for (var i = 0; i < a.length; i++) {
+                length = length + a[i] * b[i];
+            }
+            message = 'Я прошёл ' + length + ' метров';
+        } else {
+            var sum = 0;
+            for (var i = 0; i < a.length; i++) {
 
-	else if ((typeof a == "object") && (a instanceof Array)) {
-
-		if ((typeof b == "object") && (b instanceof Array)) {
-		var length = 0;
-		for(i=0;i < a.length;i++) {
-	    length = length + a[i]*b[i];
-		}
-		message = 'Я прошёл ' + length + ' метров';	
-		}
-		
-		else {
-
-			var sum=0;
-			for(i=0; i < a.length; i++) {
-				
-		    sum = sum+Math.abs(parseInt(a[i], 10));
-			}
-			message = 'Я прошёл ' + sum + ' шагов';
-		}
-	}
-
-	return message;
+                sum = sum + Math.abs(parseInt(a[i], 10));
+            }
+            message = 'Я прошёл ' + sum + ' шагов';
+        }
+    }
+    return message;
 }
