039d56e663056bfc6175916b5279ca072df81528
Michael 'Grraoo' Kosymov <evil@grraoo.ru>
2016-05-27 16:04:43 +0800

    правки
    теперь вроде все if выполняются

--- build/js/check.js
+++ build/js/check.js
@@ -1,35 +1,37 @@
-var message = ' ';
-var a;
-var b;
-
-
 var getMessage = function(a, b) {
-if(typeof(a)=='boolean') {
-	if (a == true) {
-		message = 'Я попал в ' + b;
+	var message = ' ';
+	if(typeof(a)=='boolean') {
+		if (a == true) {
+			message = 'Я попал в ' + b;
+		}
+		else  {
+			message = 'Я никуда не попал';
+		}
 	}
-	else  {
-		message = 'Я никуда не попал';
+	else if (typeof(a) == 'number') {
+		message = 'Я прыгнул на ' + a*100 + ' сантиметров';
 	}
-}
-else if (typeof(a) == 'number') {
-	message = 'Я прыгнул на ' + a*100 + ' сантиметров';
-}
 
-else if (typeof(a) == 'array') {
-	var sum = 0;
-	for(i=0;i < a.length;i++) {
-    sum = sum + a[i];
-	}
-	message = 'Я прошёл' + sum + ' шагов';
-}
+	else if ((typeof a == "object") && (a instanceof Array)) {
 
-else if ((typeof(a) == 'array')||(typeof(b) == 'array')) {
-	var length = 0;
-	for(i=0;i < a.length;i++) {
-    length = length + a[i]*b[i];
+		if ((typeof b == "object") && (b instanceof Array)) {
+		var length = 0;
+		for(i=0;i < a.length;i++) {
+	    length = length + a[i]*b[i];
+		}
+		message = 'Я прошёл ' + length + ' метров';	
+		}
+		
+		else {
+
+			var sum=0;
+			for(i=0; i < a.length; i++) {
+				
+		    sum = sum+Math.abs(parseInt(a[i], 10));
+			}
+			message = 'Я прошёл ' + sum + ' шагов';
+		}
 	}
-	message = 'Я прошёл' + length + ' метров';	
-}
-return message;
+
+	return message;
 }
