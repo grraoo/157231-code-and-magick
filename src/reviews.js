2b1648105d8941781a9cf7738eb439be012ae045
Michael 'Grraoo' Kosymov <evil@grraoo.ru>
2016-06-10 10:47:11 +0800

    Добавлена функция getJsonpRequest
    + рейтинг выставляется шириной блока, а не добавлением новых.

--- src/reviews.js
+++ src/reviews.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var reviews = [];
+window.__reviewsLoadCallback = function(data) {
+  reviews = data;
+};
+
 var reviewFilters = document.querySelector('.reviews-filter');
 var templateElement = document.querySelector('template');
 var reviewsContainer = document.querySelector('.reviews-list');
 var IMAGE_LOAD_TIMEOUT = 10000;
 var elementToClone;
+var mainJs;
+var newScript;
 
 reviewFilters.classList.add('invisible');
 
@@ -19,31 +26,27 @@ var getReviewElement = function(data, container) {
   container.appendChild(element);
 
   var ratingStar = element.querySelector('.review-rating');
-
-  if (data.rating > 1) {
-    ratingStar.style.display = 'inline-block';
-    for (var i = 0; i < data.rating - 1; i++) {
-      element.insertBefore(ratingStar.cloneNode(), ratingStar);
-    }
-  }
+  ratingStar.style.width = data.rating * 30 + 'px';
 
   element.querySelector('.review-text').textContent = data.description;
 
   var userPhoto = new Image(124, 124);
   var imageLoadTimeout;
 
-  userPhoto.onload = function() {
-    clearTimeout(imageLoadTimeout);
-  };
-
-  userPhoto.onerror = function() {
-    element.classList.add('review-load-failure');
-  };
-
   userPhoto.src = data.author.picture;
   element.querySelector('.review-author').src = userPhoto.src;
   element.querySelector('.review-author').title = data.author.name;
   element.querySelector('.review-author').alt = data.author.name;
+
+  userPhoto.onerror = function() {
+    element.classList.add('review-load-failure');
+  };
+  
+  userPhoto.onload = function() {
+    clearTimeout(imageLoadTimeout);
+  };
+  
+
 
   imageLoadTimeout = setTimeout(function() {
     userPhoto.src = '';
@@ -53,8 +56,23 @@ var getReviewElement = function(data, container) {
   return element;
 };
 
-window.reviews.forEach(function(review) {
-  getReviewElement(review, reviewsContainer);
-});
+var buildReviewList = function() {
+  reviews.forEach(function(review) {
+    getReviewElement(review, reviewsContainer);
+  });
+  reviewFilters.classList.remove('invisible');
+};
 
-reviewFilters.classList.remove('invisible');
+var getJsonpRequest = function(jsonpSource, jsonpFunction) {
+  mainJs = document.querySelector('script[src="js/main.js"]');
+
+  newScript = document.createElement('script');
+  newScript.type = 'text/javascript';
+  newScript.src = '//up.htmlacademy.ru/assets/js_intensive/jsonp/reviews.js';
+
+  document.body.insertBefore(newScript, mainJs);
+
+  newScript.onload = jsonpFunction;
+};
+
+getJsonpRequest('//up.htmlacademy.ru/assets/js_intensive/jsonp/reviews.js', buildReviewList);
